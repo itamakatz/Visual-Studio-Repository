b9c8c3ac653cc0f3cadfa773eea0f3c0affe120f
Itamar Katz <itamakatz@gmail.com>
2018-07-08 15:29:01 -0700

    Can save to SVG format succesfully!
    yet to make a bitmap.

--- try_geometry3Sharp/try_geometry3Sharp/MainWindow.xaml.cs
+++ try_geometry3Sharp/try_geometry3Sharp/MainWindow.xaml.cs
@@ -50,61 +50,52 @@ namespace try_geometry3Sharp
 			List<EdgeSpan> cutSpans = first_plane_cut.CutSpans;
 			//EdgeLoopRemesher
 
-			DCurve3 cutLoops_DCurve3 = cutLoops[0].ToCurve();
-			List<Vector2d> cutLoops_Vector2d = new List<Vector2d>();
-			DGraph2 cutLoops_DGraph2 = new DGraph2();
-
-			foreach (var item in cutLoops_DCurve3.Vertices)
-			{
-				cutLoops_Vector2d.Add(item.xz);
-				cutLoops_DGraph2.AppendVertex(item.xz);
-			}
-
-			for (int i = 0; i < cutLoops_DCurve3.VertexCount; i++)
-			{
-				cutLoops_DGraph2.AppendEdge(i, (i + 1) % cutLoops_DCurve3.VertexCount);
-			}
-
-			//cutLoops_DGraph2.AppendEdge(v_Index2i.array[0], 0);
-
-			//cutLoops_DGraph2.AllocateEdgeGroup();
-			//cutLoops_DGraph2.AppendVertex(new Vector2d(0, 0));
-			//cutLoops_DGraph2.AppendVertex(new Vector2d(50, 50));
 			SVGWriter my_SVGWriter = new SVGWriter();
-			my_SVGWriter.AddGraph(cutLoops_DGraph2);
+			my_SVGWriter.SetDefaultLineWidth(0.1f);
 
-			//Arc2d my_Arc2d = new Arc2d(new Vector2d(0, 0), 10.0, 0, 90);
-			//my_SVGWriter.AddArc(my_Arc2d);
-			//var fileStream = File.Create();
+			foreach (EdgeLoop EdgeLoop_item in cutLoops)
+			{
+				DCurve3 cutLoops_DCurve3 = EdgeLoop_item.ToCurve();
+				List<Vector2d> cutLoops_Vector2d = new List<Vector2d>();
+				DGraph2 cutLoops_DGraph2 = new DGraph2();
+
+				foreach (var item in cutLoops_DCurve3.Vertices)
+				{
+					cutLoops_Vector2d.Add(item.xz);
+					cutLoops_DGraph2.AppendVertex(item.xz);
+				}
+
+				cutLoops_DGraph2.AllocateEdgeGroup();
+				int current_group_id = cutLoops_DGraph2.MaxGroupID;
+
+				for (int i = 0; i < cutLoops_DCurve3.VertexCount; i++)
+				{
+					cutLoops_DGraph2.AppendEdge(i, (i + 1) % cutLoops_DCurve3.VertexCount, current_group_id);
+				}
+
+				my_SVGWriter.AddGraph(cutLoops_DGraph2);
+			}
+
 			my_SVGWriter.Write(SVG_PATH);
 
 			//first_plane_cut.FillHoles();
-			//MeshPlaneCut second_plane_cut = new MeshPlaneCut(first_plane_cut.Mesh, new Vector3d(1, 1, 1), new Vector3d(0, 1, 0));
-			//second_plane_cut.Cut();
+			MeshPlaneCut second_plane_cut = new MeshPlaneCut(first_plane_cut.Mesh, new Vector3d(0.1f, 0.1f, 0.1f), new Vector3d(0, 0.1f, 0));
+			second_plane_cut.Cut();
 			//second_plane_cut.FillHoles();
-			
-			//PolyLine2d BitmapExporter EdgeLoopRemesher
 
 			//IOWriteResult result =
-			//StandardMeshWriter.WriteFile(MODEL_OUT_PATH, new List<WriteMesh>() { new WriteMesh(second_plane_cut.Mesh) }, WriteOptions.Defaults);
-			StandardMeshWriter.WriteFile(MODEL_OUT_PATH, new List<WriteMesh>() { new WriteMesh(cutLoops[0].Mesh) }, WriteOptions.Defaults);
+			StandardMeshWriter.WriteFile(MODEL_OUT_PATH, new List<WriteMesh>() { new WriteMesh(second_plane_cut.Mesh) }, WriteOptions.Defaults);
 			//StandardMeshWriter.WriteFile(MODEL_OUT_PATH, new List<WriteMesh>() { new WriteMesh(meshes[0]) }, WriteOptions.Defaults);
 			//foreach (var item in meshes[0].VertexIndices())
 			//foreach (var my_DMesh3 in meshes[0].Vertices())
 			//{
 			//	//item.z
 			//}
-			
+
 			ModelVisual3D device3D = new ModelVisual3D();
 			Model3D my_Model3D = Create_3D_from_stl(MODEL_OUT_PATH);
 			device3D.Content = my_Model3D;
 
-			Viewport3D my_Viewport3D = my_3d_view.Viewport;
-			var fileStream = File.Create(PNG_PATH);
-			BitmapExporter my_BitmapExporter = new BitmapExporter();
-			//my_BitmapExporter.Export(my_Viewport3D, fileStream);
-			fileStream.Close();
-			
 			my_3d_view.RotateGesture = new MouseGesture(MouseAction.LeftClick);
 			my_3d_view.Children.Add(device3D);
 		}
