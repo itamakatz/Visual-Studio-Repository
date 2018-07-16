a9f002adbbc1b36dedade6e9121bf69418c132d8
itamakatz <itamakatz@gmail.com>
2018-07-16 14:22:13 +0300

    added the foundation to work with only byte arrays instead of Birmap
    for complexity reasons

--- IOTech_BitMap_Slicer/IOTech_BitMap_Slicer/Bitmap_Slice.cs
+++ IOTech_BitMap_Slicer/IOTech_BitMap_Slicer/Bitmap_Slice.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 //using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -14,6 +15,10 @@ namespace IOTech_BitMap_Slicer
 	class Bitmap_Slice
 	{
 		public Bitmap bitmap { get; }
+		public byte[] bitmap_byte_array;
+
+		private byte byte_map_value = 100;
+
 		public static int SCALE_FACTOR;
 		public static int PEN_FINE_WIDTH { get; set; }
 		public static int PEN_ROBUST_WIDTH { get; set; }
@@ -24,7 +29,8 @@ namespace IOTech_BitMap_Slicer
 
 		private bool[] bool_array;
 		private int current_position;
-		private int bitmap_width;
+		public int bitmap_width { get; set; }
+		public int bitmap_height { get; set; }
 
 		public static int recursive_count = 0;
 		
@@ -33,14 +39,18 @@ namespace IOTech_BitMap_Slicer
 			bitmap = new Bitmap(get_int_dimension(width), get_int_dimension(height));
 			graphics = Graphics.FromImage(bitmap);
 			bitmap_width = bitmap.Width;
-			bool_array = new bool[bitmap.Height * bitmap_width];
+			bitmap_height = bitmap.Height;
+			bool_array = new bool[bitmap_height * bitmap_width];
+			//bitmap_byte_array = new byte[bitmap_height * bitmap_width];
 		}
 
 		public Bitmap_Slice(Bitmap bitmap)
 		{
 			this.bitmap = bitmap;
 			bitmap_width = bitmap.Width;
-			bool_array = new bool[bitmap.Height * bitmap_width];
+			bitmap_height = bitmap.Height;
+			bool_array = new bool[bitmap_height * bitmap_width];
+			//bitmap_byte_array = Array1DFromBitmap(bitmap);
 		}
 
 		public void flood_fill_recursive()
@@ -56,6 +66,10 @@ namespace IOTech_BitMap_Slicer
 			{
 				bitmap.SetPixel(current_x, current_y, bitmap_color);
 			}
+			//if (bitmap_byte_array[current_x + current_y * bitmap_width] == 0)
+			//{
+			//	//bitmap_byte_array[current_x + current_y * bitmap_width] = byte_map_value;
+			//}
 			else
 			{
 				return;
@@ -77,7 +91,7 @@ namespace IOTech_BitMap_Slicer
 					flood_fill_recursive();
 				}
 			}
-			if (current_x < bitmap.Size.Width - 1)
+			if (current_x < bitmap_width - 1)
 			{
 				current_position = (current_x + 1) + current_y * bitmap_width;
 				if (!bool_array[current_position])
@@ -85,7 +99,7 @@ namespace IOTech_BitMap_Slicer
 					flood_fill_recursive();
 				}
 			}
-			if (current_y < bitmap.Size.Height - 1)
+			if (current_y < bitmap_height - 1)
 			{
 				current_position = current_x + (current_y + 1) * bitmap_width;
 				if (!bool_array[current_position])
@@ -97,6 +111,21 @@ namespace IOTech_BitMap_Slicer
 
 		public void set_starting_point(int width, int height)
 		{
+			try
+			{
+				int i = width;
+				while(!check_color_equal(bitmap.GetPixel(i, height), bitmap_color)) { i--; }
+				i = height;
+				while(!check_color_equal(bitmap.GetPixel(width, i), bitmap_color)) { i--; }
+				i = width;
+				while(!check_color_equal(bitmap.GetPixel(width, i), bitmap_color)) { i++; }
+				i = width;
+				while(!check_color_equal(bitmap.GetPixel(i, height), bitmap_color)) { i++; }
+			}
+			catch (Exception)
+			{
+				throw new System.ArgumentException("Starting coordiantes for flodd fill are not bound");
+			}
 			current_position = width + height * bitmap_width;
 		}
 
@@ -107,13 +136,83 @@ namespace IOTech_BitMap_Slicer
 
 		public void DrawLineInt(Vector2d origin_vec, Vector2d dest_vec)
 		{
-			graphics.DrawLine(Pen_Fine, (float)origin_vec.x, (float)(bitmap.Size.Height - origin_vec.y),
-							(float)dest_vec.x, (float)(bitmap.Size.Height - dest_vec.y));
+			graphics.DrawLine(Pen_Fine, (float)origin_vec.x, (float)(bitmap_height - origin_vec.y),
+							(float)dest_vec.x, (float)(bitmap_height - dest_vec.y));
+			//DrawLine_byte_array(origin_vec, dest_vec);
+		}
+
+		public void DrawLine_byte_array(Vector2d origin_vec, Vector2d dest_vec)
+		{ 
+			int x0 = (int) Math.Floor(origin_vec.x);
+			int y0 = (int) Math.Floor(origin_vec.y);
+			int x1 = (int) Math.Ceiling(dest_vec.x);
+			int y1 = (int) Math.Ceiling(dest_vec.y);
+
+			int dx = Math.Abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
+			int dy = Math.Abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
+			int err = (dx > dy ? dx : -dy) / 2, e2;
+
+			while (true)
+			{
+				bitmap_byte_array[x0 + y0 * bitmap_width] = byte_map_value;
+				if (x0 == x1 && y0 == y1) break;
+				e2 = err;
+				if (e2 > -dx) { err -= dy; x0 += sx; }
+				if (e2 < dy) { err += dx; y0 += sy; }
+			}
+		}
+
+		public Bitmap get_bitmap_from_byte()
+		{
+			return BitmapFromArray1D(bitmap_byte_array, bitmap_width, bitmap_height);
 		}
 
 		private Int32 get_int_dimension(double in_double)
 		{
 			return (Int32)Math.Ceiling(in_double + PEN_ROBUST_WIDTH * 2) * SCALE_FACTOR;
+		}
+
+		public static byte[] Array1DFromBitmap(Bitmap bmp)
+		{
+			if (bmp == null) throw new NullReferenceException("Bitmap is null");
+
+			System.Drawing.Rectangle rect = new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height);
+			BitmapData data = bmp.LockBits(rect, ImageLockMode.ReadWrite, bmp.PixelFormat);
+			IntPtr ptr = data.Scan0;
+
+			//declare an array to hold the bytes of the bitmap
+			int numBytes = data.Stride * bmp.Height;
+			byte[] bytes = new byte[numBytes];
+
+			//copy the RGB values into the array
+			System.Runtime.InteropServices.Marshal.Copy(ptr, bytes, 0, numBytes);
+
+			bmp.UnlockBits(data);
+
+			return bytes;
+		}
+
+		public static Bitmap BitmapFromArray1D(byte[] bytes, int width, int height)
+		{
+			Bitmap grayBmp = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format8bppIndexed);
+			System.Drawing.Rectangle grayRect = new System.Drawing.Rectangle(0, 0, grayBmp.Width, grayBmp.Height);
+			BitmapData grayData = grayBmp.LockBits(grayRect, ImageLockMode.ReadWrite, grayBmp.PixelFormat);
+			IntPtr grayPtr = grayData.Scan0;
+
+			int grayBytes = grayData.Stride * grayBmp.Height;
+			ColorPalette pal = grayBmp.Palette;
+
+			for (int g = 0; g < 256; g++)
+			{
+				pal.Entries[g] = System.Drawing.Color.FromArgb(g, g, g);
+			}
+
+			grayBmp.Palette = pal;
+
+			System.Runtime.InteropServices.Marshal.Copy(bytes, 0, grayPtr, grayBytes);
+
+			grayBmp.UnlockBits(grayData);
+			return grayBmp;
 		}
 
 		internal void Draw_rectangle(int width, int height)
--- IOTech_BitMap_Slicer/IOTech_BitMap_Slicer/MainWindow.xaml.cs
+++ IOTech_BitMap_Slicer/IOTech_BitMap_Slicer/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿#define PAINT_BITMAP_BORDERS
+﻿//#define PAINT_BITMAP_BORDERS
 //#define RUN_VISUAL
 #define SHOW_STARTING_POINT
 #define SHOW_LOCATION_OF_FLOOD_STARTING_POINT
@@ -10,14 +10,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Media.Media3D;
 using System.Drawing;
 using HelixToolkit.Wpf;
@@ -25,8 +17,6 @@ using System.IO;
 using System.Diagnostics;
 using System.Threading;
 using System.Drawing.Imaging;
-//using PictureBoxScroll;
-//using FloodFill2;
 
 namespace IOTech_BitMap_Slicer
 {
@@ -35,18 +25,11 @@ namespace IOTech_BitMap_Slicer
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		// ****
-		// should chane all paths to be relative using:
-		// string filePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\sample.svg");
 
 		// ***** Variables that can be changed ***** //
 
-
 		private string MODEL_IN_PATH = @"Desktop\CorvinCastle.stl";
 		private string MODEL_OUT_PATH = @"Desktop\CorvinCastle_new.stl";
-
-		private string SVG_DIR_PREFIX = @"Desktop\SVG_slice";
-		private const string SVG_PATH_SUFIX = @".SVG";
 
 		private string BITMAP_DIR_PREFIX = @"Desktop\BITMAP_slice";
 		private const string BITMAP_PATH_SUFIX = @".Bmp";
@@ -54,36 +37,30 @@ namespace IOTech_BitMap_Slicer
 
 		private string TMP_DIR = @"Desktop\TMP Directory";
 
-		private const int SCALE_FACTOR = 10;
+		private const int SCALE_FACTOR = 2;
 		private const double NUM_OF_SLICES = 4;
 		private const Axis SLICING_AXIS = Axis.Y;
 		
-		private const float SVG_WIDTH = 0.1f;
 		private const int EXIT_CODE = 10;
 
-		private const int PEN_FINE_WIDTH = 3;
+		private const int PEN_FINE_WIDTH = 1;
 		private const int PEN_ROBUST_WIDTH = 3;
 
 		private static System.Drawing.Color bitmap_color = System.Drawing.Color.Blue;
 
-		//System.Drawing.Pen blackPen = new System.Drawing.Pen(System.Drawing.Color.Black, PEN_WIDTH );
 		private static System.Drawing.Pen Pen_Robust = new System.Drawing.Pen(bitmap_color, PEN_ROBUST_WIDTH);
 		private static System.Drawing.Pen Pen_Fine = new System.Drawing.Pen(bitmap_color, PEN_FINE_WIDTH);
 
-
+		//private const Int32 stackSize = 2147483647;  // max Int32 = 2147483647 
+		private const Int32 stackSize = 1000000000;  // max Int32 = 2147483647 
 
 		// ***** Initialization of other variables ***** //
 
 		private string USER_PATH = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
 		 
-		private Vector3d SLICING_DIRECTION_UNIT;
 		private Vector2d Bitmap_dimensions = new Vector2d();
 		private Vector2d Mesh_min_dimensions = new Vector2d();
 		
-		private IEnumerable<double> Slice_Enumerator;
-
-		private DMesh3 Imported_STL_mesh;
-		private Model3D HelixToolkit_Model3D = null;
 		private Bitmap_Slice bitmap_slice;
 
 		internal Stopwatch watch = new Stopwatch();
@@ -94,9 +71,14 @@ namespace IOTech_BitMap_Slicer
 
 		public MainWindow()
 		{
+			// initialization and declaration of variables
 			InitializeComponent();
 
 			Check_directories();
+
+			DMesh3 Imported_STL_mesh = null;
+			Model3D HelixToolkit_Model3D = null;
+			Vector3d SLICING_DIRECTION_UNIT;
 
 			Bitmap_Slice.SCALE_FACTOR = SCALE_FACTOR;
 			Bitmap_Slice.bitmap_color = bitmap_color;
@@ -104,6 +86,7 @@ namespace IOTech_BitMap_Slicer
 			Bitmap_Slice.PEN_ROBUST_WIDTH = PEN_ROBUST_WIDTH;
 			Bitmap_Slice.Pen_Fine = new System.Drawing.Pen(bitmap_color, PEN_FINE_WIDTH);
 			Bitmap_Slice.Pen_Robust = new System.Drawing.Pen(bitmap_color, PEN_ROBUST_WIDTH);
+
 			Trace.WriteLine("wasssup broooo????");
 
 			// Open STL file and create DMesh3 objects
@@ -116,15 +99,14 @@ namespace IOTech_BitMap_Slicer
 			}
 			else
 			{
-				Trace.WriteLine("USER_PATH is - " + USER_PATH);
-				Trace.WriteLine("MODEL_IN_PATH is - " + MODEL_IN_PATH);
-				Trace.WriteLine("DMesh3Builder faild to open stl model");
-				Environment.Exit(EXIT_CODE);
+				exit_messege(new string[] { "DMesh3Builder faild to open stl model" ,
+					"USER_PATH is - " + USER_PATH ,
+					"MODEL_IN_PATH is - " + MODEL_IN_PATH });
 			}
 
 			Vector3d STL_mesh_Diagonal = Imported_STL_mesh.CachedBounds.Diagonal;
 
-			Slice_Enumerator = Range_Enumerator(Imported_STL_mesh.CachedBounds.Min[(int)SLICING_AXIS],
+			IEnumerable<double> Slice_Enumerator = Range_Enumerator(Imported_STL_mesh.CachedBounds.Min[(int)SLICING_AXIS],
 									Imported_STL_mesh.CachedBounds.Max[(int)SLICING_AXIS],
 									STL_mesh_Diagonal[(int)SLICING_AXIS] / NUM_OF_SLICES);
 
@@ -176,8 +158,6 @@ namespace IOTech_BitMap_Slicer
 				plane_cut_cross_section = new MeshPlaneCut(new DMesh3(Imported_STL_mesh), SLICING_DIRECTION_UNIT * slice_step, SLICING_DIRECTION_UNIT);
 				plane_cut_cross_section.Cut();
 				Create_Bitmap(plane_cut_cross_section);
-
-				//Create_SVG(plane_cut_cross_section);
 			}
 
 			watch.Reset();
@@ -187,11 +167,12 @@ namespace IOTech_BitMap_Slicer
 
 			watch.Stop();
 
-			//flood_fill_bitmap();
-			//my_flood_fill_botmap();
+
+#if RUN_VISUAL
+
 			//second_cross_section = new MeshPlaneCut(main_cross_section.Mesh, plane_origine - 0.01F, SLICING_NORMAL * -1);
 			//second_cross_section.Cut();
-#if RUN_VISUAL
+
 			StandardMeshWriter.WriteFile(MODEL_OUT_PATH, new List<WriteMesh>() { new WriteMesh(plane_cut_cross_section.Mesh) }, WriteOptions.Defaults);
 
 			// Using HelixToolkit show 3D object on GUI
@@ -203,7 +184,8 @@ namespace IOTech_BitMap_Slicer
 			}
 			catch (Exception e)
 			{
-				MessageBox.Show("Exception Error : " + e.StackTrace);
+				// MessageBox.Show("Exception Error : " + e.StackTrace);
+				exit_messege(new string[] { "Exception loading HelixToolkit Model3D" }, e);
 			}
 
 #if SHOW_BOUNDS
@@ -242,39 +224,7 @@ namespace IOTech_BitMap_Slicer
 			my_3d_view.Children.Add(device3D);
 #endif
 			Trace.WriteLine(Bitmap_Slice.recursive_count);
-			TimeSpan ts = watch.Elapsed;
-			string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-			ts.Hours, ts.Minutes, ts.Seconds,
-			ts.Milliseconds / 10);
-			Trace.WriteLine("RunTime: " + elapsedTime);
-		}
-
-		private void flood_fill_bitmap()
-		{
-			bitmap_slice = new Bitmap_Slice(new Bitmap(BITMAP_DIR_PREFIX + @"\" + "slice_" + "2" + "_loop_" + "1" + BITMAP_PATH_SUFIX));
-
-			int begin_x = bitmap_slice.bitmap.Width / 2;
-			int begin_y = bitmap_slice.bitmap.Height / 2;
-			int mark_length = 20;
-
-			var stackSize = 100000000;  // max Int32 = 2147483648 
-			bitmap_slice.set_starting_point(begin_x, begin_y);
-
-			Thread thread = new Thread(new ThreadStart(bitmap_slice.flood_fill_recursive), stackSize);
-			thread.Start();
-			thread.Join();
-			bitmap_slice.bitmap.Save(BITMAP_DIR_PREFIX + @"\" + "flood_fill_Bitmap" + BITMAP_PATH_SUFIX, image_format);
-
-#if SHOW_LOCATION_OF_FLOOD_STARTING_POINT
-			Bitmap bitmap_flood = new Bitmap(BITMAP_DIR_PREFIX + @"\" + "slice_" + "2" + "_loop_" + "1" + BITMAP_PATH_SUFIX);
-
-			using (var graphics = Graphics.FromImage(bitmap_flood))
-			{
-				graphics.DrawLine(new System.Drawing.Pen(System.Drawing.Color.Yellow, PEN_ROBUST_WIDTH), begin_x - mark_length, begin_y, begin_x + mark_length, begin_y);
-				graphics.DrawLine(new System.Drawing.Pen(System.Drawing.Color.Yellow, PEN_ROBUST_WIDTH), begin_x, begin_y - mark_length, begin_x, begin_y + mark_length);
-			}
-			bitmap_flood.Save(BITMAP_DIR_PREFIX + @"\" + "slice_" + "2" + "_loop_" + "1_2" + BITMAP_PATH_SUFIX, image_format);
-#endif
+			print_elapsed(watch.Elapsed);
 		}
 
 		private void Create_Bitmap(MeshPlaneCut cross_section)
@@ -310,81 +260,74 @@ namespace IOTech_BitMap_Slicer
 
 			try
 			{
-				bitmap_slice.bitmap.Save(BITMAP_DIR_PREFIX + @"\" + "slice_" + (Slice_Count) + "_loop_" + (loop_count++) + BITMAP_PATH_SUFIX,
-						image_format);
+				//bitmap_slice.get_bitmap_from_byte().Save(BITMAP_DIR_PREFIX + @"\" + "slice_" + (Slice_Count) + "_loop_" + (loop_count++) + BITMAP_PATH_SUFIX, image_format);
+				bitmap_slice.bitmap.Save(BITMAP_DIR_PREFIX + @"\" + "slice_" + (Slice_Count) + "_loop_" + (loop_count++) + BITMAP_PATH_SUFIX, image_format);
 			}
-			catch (Exception)
+			catch (Exception e)
 			{
-				throw;
+				exit_messege(new string[] { "Error saving bitmap" }, e);
 			}
-			
+
 			Slice_Count++;
 		}
 
-		public IEnumerable<double> Range_Enumerator(double start, double end, double increment)
+		private void flood_fill_bitmap()
+		{
+			bitmap_slice = new Bitmap_Slice(new Bitmap(BITMAP_DIR_PREFIX + @"\" + "slice_" + "2" + "_loop_" + "1" + BITMAP_PATH_SUFIX));
+
+			int begin_x = bitmap_slice.bitmap.Width / 2;
+			int begin_y = bitmap_slice.bitmap.Height / 2;
+			int mark_length = 3;
+
+#if SHOW_LOCATION_OF_FLOOD_STARTING_POINT
+			using (var graphics = Graphics.FromImage(bitmap_slice.bitmap))
+			{
+				graphics.DrawLine(new System.Drawing.Pen(System.Drawing.Color.Yellow, PEN_FINE_WIDTH), begin_x - mark_length, begin_y - mark_length, begin_x + mark_length, begin_y + mark_length);
+				graphics.DrawLine(new System.Drawing.Pen(System.Drawing.Color.Yellow, PEN_FINE_WIDTH), begin_x + mark_length, begin_y - mark_length, begin_x - mark_length, begin_y + mark_length);
+			}
+			bitmap_slice.bitmap.Save(BITMAP_DIR_PREFIX + @"\" + "slice_" + "2" + "_loop_" + "1_2" + BITMAP_PATH_SUFIX, image_format);
+
+			bitmap_slice = new Bitmap_Slice(new Bitmap(BITMAP_DIR_PREFIX + @"\" + "slice_" + "2" + "_loop_" + "1" + BITMAP_PATH_SUFIX));
+#endif
+			try
+			{
+				bitmap_slice.set_starting_point(begin_x, begin_y);
+				Thread thread = new Thread(new ThreadStart(bitmap_slice.flood_fill_recursive), stackSize);
+				thread.Start();
+				thread.Join();
+
+				//bitmap_slice.get_bitmap_from_byte().Save(BITMAP_DIR_PREFIX + @"\" + "flood_fill_Bitmap" + BITMAP_PATH_SUFIX, image_format);
+				bitmap_slice.bitmap.Save(BITMAP_DIR_PREFIX + @"\" + "flood_fill_Bitmap" + BITMAP_PATH_SUFIX, image_format);
+			}
+			catch (ArgumentException e)
+			{
+				exit_messege(new string[] { "Starting point of flood fill out of bounds.", "begin_x: " + begin_x, "begin_y: " + begin_y }, e);
+			}
+			catch (Exception e)
+			{
+				exit_messege(new string[] { "Faild to flood fill bitmap" }, e);
+			}
+		}
+
+		private IEnumerable<double> Range_Enumerator(double start, double end, double increment)
 		{
 			for (double i = start + increment; i < end; i += increment)
 				yield return i;
-		}
-
-		public static byte[] Array1DFromBitmap(Bitmap bmp)
-		{
-			if (bmp == null) throw new NullReferenceException("Bitmap is null");
-
-			System.Drawing.Rectangle rect = new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height);
-			BitmapData data = bmp.LockBits(rect, ImageLockMode.ReadWrite, bmp.PixelFormat);
-			IntPtr ptr = data.Scan0;
-
-			//declare an array to hold the bytes of the bitmap
-			int numBytes = data.Stride * bmp.Height;
-			byte[] bytes = new byte[numBytes];
-
-			//copy the RGB values into the array
-			System.Runtime.InteropServices.Marshal.Copy(ptr, bytes, 0, numBytes);
-
-			bmp.UnlockBits(data);
-
-			return bytes;
-		}
-
-		public static Bitmap BitmapFromArray1D(byte[] bytes, int width, int height)
-		{
-			Bitmap grayBmp = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format8bppIndexed);
-			System.Drawing.Rectangle grayRect = new System.Drawing.Rectangle(0, 0, grayBmp.Width, grayBmp.Height);
-			BitmapData grayData = grayBmp.LockBits(grayRect, ImageLockMode.ReadWrite, grayBmp.PixelFormat);
-			IntPtr grayPtr = grayData.Scan0;
-
-			int grayBytes = grayData.Stride * grayBmp.Height;
-			ColorPalette pal = grayBmp.Palette;
-
-			for (int g = 0; g < 256; g++)
-			{
-				pal.Entries[g] = System.Drawing.Color.FromArgb(g, g, g);
-			}
-
-			grayBmp.Palette = pal;
-
-			System.Runtime.InteropServices.Marshal.Copy(bytes, 0, grayPtr, grayBytes);
-
-			grayBmp.UnlockBits(grayData);
-			return grayBmp;
 		}
 
 		void Check_directories()
 		{
 			try
 			{
+				// ****
+				// maybe switch to this relative path
+				//string filePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\sample.svg");
+
 				// adjust all path's relative to specific pc
 				MODEL_IN_PATH = System.IO.Path.Combine(USER_PATH, MODEL_IN_PATH);
 				MODEL_OUT_PATH = System.IO.Path.Combine(USER_PATH, MODEL_OUT_PATH);
-				SVG_DIR_PREFIX = System.IO.Path.Combine(USER_PATH, SVG_DIR_PREFIX);
 				BITMAP_DIR_PREFIX = System.IO.Path.Combine(USER_PATH, BITMAP_DIR_PREFIX);
 				TMP_DIR = System.IO.Path.Combine(USER_PATH, TMP_DIR);
-
-				if (!File.Exists(SVG_DIR_PREFIX))
-				{
-					Directory.CreateDirectory(SVG_DIR_PREFIX);
-				}
 
 				if (!File.Exists(BITMAP_DIR_PREFIX))
 				{
@@ -398,9 +341,7 @@ namespace IOTech_BitMap_Slicer
 			}
 			catch (Exception e)
 			{
-				Trace.WriteLine("Check_directories faild");
-				Trace.WriteLine(e);
-				Environment.Exit(EXIT_CODE);
+				exit_messege(new string[] { "Check_directories faild" }, e);
 			}
 		}
 
@@ -415,6 +356,49 @@ namespace IOTech_BitMap_Slicer
 			{
 				dir.Delete(true);
 			}
+		}
+
+		public static void exit_messege(string[] messages)
+		{
+			// possibly use:
+			// MessageBox.Show("Exception Error : " + e.StackTrace);
+			Trace.WriteLine("\n");
+
+			foreach (string msg in messages)
+			{
+				Trace.WriteLine(msg);
+			}
+
+			Trace.WriteLine("\n");
+
+			Environment.Exit(EXIT_CODE);
+		}
+
+		public static void exit_messege(string[] messages, Exception e)
+		{
+			// possibly use:
+			// MessageBox.Show("Exception Error : " + e.StackTrace);
+			Trace.WriteLine("\n");
+
+			foreach (string msg in messages)
+			{
+				Trace.WriteLine(msg);
+			}
+
+			Trace.WriteLine("\n");
+
+			Trace.WriteLine(e.StackTrace);
+			Trace.WriteLine(e.Message);
+
+			Environment.Exit(EXIT_CODE);
+		}
+
+		private static void print_elapsed(TimeSpan time_stamp)
+		{
+			string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+			time_stamp.Hours, time_stamp.Minutes, time_stamp.Seconds,
+			time_stamp.Milliseconds / 10);
+			Trace.WriteLine("RunTime: " + elapsedTime);
 		}
 	}
 }
