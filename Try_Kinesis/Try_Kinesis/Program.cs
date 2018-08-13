c81253fe08d361d325324e7fe01cd652ef6d2453
itamakatz <itamakatz@gmail.com>
2018-08-13 12:39:02 +0300

    moving forward slowly but surely

--- Try_Kinesis/Try_Kinesis/Program.cs
+++ Try_Kinesis/Try_Kinesis/Program.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using Thorlabs.MotionControl.DeviceManagerCLI;
 using Thorlabs.MotionControl.KCube.DCServoCLI;
 
@@ -11,20 +9,42 @@ namespace Try_Kinesis
 	class Program
 	{
 		//private static KCubeDCServo _kCubeDCServoMotor = null;
-		private static KCubeDCServo _kCubeDCServoMotor = new KCubeDCServo();
+		//private static KCubeDCServo _kCubeDCServoMotor;
+
+		private const string serialNumber = "27000961";
+
+		static KCubeDCServo _kCubeDCServoMotor;
+
+		const int DEFAULTVEL = 10;
+		const int DEFAULTACC = 10;
+		const int TPOLLING = 250;
+		const int TIMEOUTSETTINGS = 7000;
+		const int TIMEOUTMOVE = 100000;
+
+		//void run()
+		//{
+		//	KCubeDCServo.CreateKCubeDCServo(serialNumber);
+		//}
 
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Ready to start!");
-			Console.ReadKey();
 
-			if (_kCubeDCServoMotor != null)
-			{
-				Console.WriteLine("Device already connected");
-				return;
-			}
+			
+			Program program = new Program();
 
-			const string serialNumber = "27250312";
+			//program .run();
+
+			//DeviceManagerCLI.BuildDeviceList();
+
+			//KCubeDCServo _kCubeDCServoMotor;
+			//_kCubeDCServoMotor = KCubeDCServo.CreateKCubeDCServo(serialNumber);
+			//KCubeDCServo.CreateKCubeDCServo(serialNumber);
+
+			//if (_kCubeDCServoMotor != null)
+			//{
+			//	Console.WriteLine("Device already connected");
+			//	return;
+			//}
 
 			// All of this operation has been placed inside a single "catch-all"
 			// exception handler. This is to reduce the size of the example code.
@@ -36,11 +56,35 @@ namespace Try_Kinesis
 				// Instructs the DeviceManager to build and maintain the list of
 				// devices connected.
 				DeviceManagerCLI.BuildDeviceList();
+				List<string> device_list = DeviceManagerCLI.GetDeviceList();
 
 				_kCubeDCServoMotor = KCubeDCServo.CreateKCubeDCServo(serialNumber);
 
-				// Establish a connection with the device.
-				_kCubeDCServoMotor.Connect(serialNumber);
+				_kCubeDCServoMotor.ClearDeviceExceptions();
+
+				if (!_kCubeDCServoMotor.IsSettingsInitialized())
+				{
+					//Establish a connection with the device.
+					_kCubeDCServoMotor.Connect(serialNumber);
+				}
+
+				if (!_kCubeDCServoMotor.IsSettingsInitialized())
+				{
+					//Establish a connection with the device.
+					WriteLine("Unable to initialise device" + serialNumber);
+				}
+
+				_kCubeDCServoMotor.StartPolling(TPOLLING);
+
+				var motorSettings = _kCubeDCServoMotor.GetMotorConfiguration(serialNumber, DeviceConfiguration.DeviceSettingsUseOptionType.UseDeviceSettings);
+
+				Thorlabs.MotionControl.GenericMotorCLI.Settings.MotorDeviceSettings currentDeviceSettingsNET = _kCubeDCServoMotor.MotorDeviceSettings;
+				var deviceInfoNET = _kCubeDCServoMotor.GetDeviceInfo();
+				var MotDi = Thorlabs.MotionControl.GenericMotorCLI.Settings.RotationSettings.RotationDirections;
+				//.RotationDirection.Forward;
+				currentDeviceSettingsNET.Rotation.RotationDirection;
+				//= MotDi;
+				//.Rotation.RotationDirection = MotDir; 
 
 				// Wait for the device settings to initialize. We ask the device to
 				// throw an exception if this takes more than 5000ms (5s) to complete.
@@ -59,20 +103,40 @@ namespace Try_Kinesis
 			}
 			catch (Exception ex)
 			{
-				Console.WriteLine("Unable to connect to device\n" + ex);
+				WriteLine("Unable to connect to device\n" + ex);
 			}
 
-			Console.WriteLine("Holly shit it worked.... now to move!!");
+			//Console.WriteLine("Holly shit it worked.... now to move!!");
+			//Console.ReadKey();
+			try
+			{
+				_kCubeDCServoMotor.IdentifyDevice();
+				Decimal max_vel = _kCubeDCServoMotor.GetVelocityParams().MaxVelocity;
+				//WriteLine(max_vel.ToString());
+				WriteLine(max_vel + "");
+			}
+			catch (Exception ex)
+			{
+				WriteLine("Unable to connect to device\n" + ex);
+			}
+			//_kCubeDCServoMotor.MoveTo(0, 0);
+			//_kCubeDCServoMotor.SetJogStepSize(10);
+			//_kCubeDCServoMotor.MoveJog(Thorlabs.MotionControl.GenericMotorCLI.MotorDirection.Forward, 0);
+
+			//_kCubeDCServoMotor.MoveTo(50, 0);
+
+			WriteLine("end");
 			Console.ReadKey();
 
-			_kCubeDCServoMotor.MoveTo(0, 0);
+			_kCubeDCServoMotor.DisableDevice();
+			_kCubeDCServoMotor.StopPolling();
+			_kCubeDCServoMotor.Disconnect(true);
+		}
 
-			_kCubeDCServoMotor.MoveTo(10, 10000);
-
-
-			Console.WriteLine("ohhhh baby");
-			Console.ReadKey();
-
+		static void WriteLine(string str)
+		{
+			Trace.WriteLine(str);
+			Console.WriteLine(str);
 		}
 	}
 }
