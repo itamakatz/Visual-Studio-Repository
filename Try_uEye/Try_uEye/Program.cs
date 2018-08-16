13632cd52775ff9d2642c4cd3a7198dce74862f2
itamakatz <itamakatz@gmail.com>
2018-08-16 19:04:01 +0300

    Was able to show a video live of the microscopefrom the console itself!!!!

--- Try_uEye/Try_uEye/Program.cs
+++ Try_uEye/Try_uEye/Program.cs
@@ -6,6 +6,8 @@ using System.Drawing.Imaging;
 using System.Threading;
 using System.Diagnostics;
 using uEye;
+using System.IO;
+using System.Timers;
 
 namespace Try_uEye
 {
@@ -15,10 +17,12 @@ namespace Try_uEye
 		const string SAVE_PATH = @"C:\Users\admin\Desktop\COM_Integration\uEye\";
 		const string SAVE_PATH_SUFFIX = @".PNG";
 
-		private uEye.Camera camera;
 		IntPtr displayHandle = IntPtr.Zero;
-		private bool bLive = false;
 
+		int capture_wait_time = 10;
+		int timer_elapse = 50;
+
+		private static System.Timers.Timer capture_timer;
 
 		static void Main(string[] args)
 		{
@@ -27,7 +31,6 @@ namespace Try_uEye
 
 			Program p = new Program();
 			p.My_Init_Camera();
-			//p.Old_Program();
 
 			Console.WriteLine("Program Finished");
 			Console.ReadKey();
@@ -36,118 +39,56 @@ namespace Try_uEye
 		private void My_Init_Camera()
 		{
 
-			uEye.Camera cam = new uEye.Camera();
+			Camera cam = new Camera();
 			cam.Init();
 
-			uEye.Configuration.BootBoost.Wait();
-
 			displayHandle = Process.GetCurrentProcess().MainWindowHandle;
-			
+
 			cam.Memory.Allocate();
 
-			int s32Wait = 500;
-
-			cam.Acquisition.Capture(s32Wait);
-
-			//cam.Acquisition.Capture();
-
-			uEye.Defines.DeviceParameter la = uEye.Defines.DeviceParameter.DontWait;
-			//uEye.Defines.DeviceParameter la = uEye.Defines.DeviceParameter.Wait;
-
-			cam.Acquisition.Freeze(la);
+			cam.Acquisition.Capture(capture_wait_time);
 
 			Int32 s32MemID;
 			cam.Memory.GetActive(out s32MemID);
+			cam.EventFrame += Render_Event;
+
+			capture_timer = new System.Timers.Timer(timer_elapse);
+
+			capture_timer.Elapsed += (sender, e) => Capture_Event(sender, e, ref cam);
+			capture_timer.AutoReset = true;
+			capture_timer.Enabled = true;
+
+			Console.ReadKey();
 
 			cam.Display.Render(s32MemID, displayHandle, uEye.Defines.DisplayRenderMode.FitToWindow);
 
-			//cam.Display.Render(mode, handle);
+			save_iamge(ref cam);
 
-			cam.Image.Save(SAVE_PATH + "image" + SAVE_PATH_SUFFIX, ImageFormat.Png);
-
+			capture_timer.Stop();
+			capture_timer.Dispose();
 		}
 
-
-		private void Old_Program()
+		private void Render_Event(object sender, EventArgs e)
 		{
+			Camera Camera = sender as Camera;
 
-			uEye.Camera cam = new uEye.Camera();
-			cam.Init();
+			Int32 s32MemID;
+			Camera.Memory.GetActive(out s32MemID);
 
-			uEye.Configuration.BootBoost.Wait();
-
-			cam.Memory.Allocate();
-
-			//cam.Acquisition.Capture();
-
-			int s32Wait = 500;
-
-			cam.Acquisition.Capture(s32Wait);
-
-			uEye.Defines.DeviceParameter la = uEye.Defines.DeviceParameter.DontWait;
-			//uEye.Defines.DeviceParameter la = uEye.Defines.DeviceParameter.Wait;
-
-			cam.Acquisition.Freeze(la);
-
-			uEye.Defines.DisplayRenderMode mode = uEye.Defines.DisplayRenderMode.FitToWindow;
-
-			cam.Display.Render(mode);
-
-			cam.Image.Save(SAVE_PATH + "image" + SAVE_PATH_SUFFIX, ImageFormat.Png);
-
-			//Console.WriteLine("Finished running code!\n");
-			//Console.ReadKey();
+			Camera.Display.Render(s32MemID, displayHandle, uEye.Defines.DisplayRenderMode.FitToWindow);
 		}
 
+		private void Capture_Event(object sender, EventArgs e, ref Camera cam)
+		{
+			cam.Acquisition.Capture(capture_wait_time);
+		}
 
-		//private void InitCamera()
-		//{
-		//	camera = new uEye.Camera();
+		private static void save_iamge(ref Camera cam)
+		{
+			int i = 0;
+			while (File.Exists(SAVE_PATH + "uEye_image_" + i + SAVE_PATH_SUFFIX)) { i++; }
 
-		//	uEye.Defines.Status statusRet = 0;
-
-		//	// Open Camera
-		//	statusRet = camera.Init();
-		//	if (statusRet != uEye.Defines.Status.Success)
-		//	{
-		//		Console.WriteLine("Camera initializing failed");
-		//		Environment.Exit(-1);
-		//	}
-
-		//	// Allocate Memory
-		//	statusRet = camera.Memory.Allocate();
-		//	if (statusRet != uEye.Defines.Status.Success)
-		//	{
-		//		Console.WriteLine("Allocate Memory failed");
-		//		Environment.Exit(-1);
-		//	}
-
-		//	// Start Live Video
-		//	statusRet = camera.Acquisition.Capture();
-		//	if (statusRet != uEye.Defines.Status.Success)
-		//	{
-		//		Console.WriteLine("Start Live Video failed");
-		//	}
-		//	else
-		//	{
-		//		bLive = true;
-		//	}
-
-		//	// Connect Event
-		//	camera.EventFrame += onFrameEvent;
-
-		//	//CB_Auto_Gain_Balance.Enabled = camera.AutoFeatures.Software.Gain.Supported;
-		//	//CB_Auto_White_Balance.Enabled = camera.AutoFeatures.Software.WhiteBalance.Supported;
-		//}
-
-		//private void onFrameEvent(object sender, EventArgs e)
-		//{
-		//	uEye.Camera Camera = sender as uEye.Camera;
-
-		//	Int32 s32MemID;
-		//	Camera.Memory.GetActive(out s32MemID);
-
-		//	Camera.Display.Render(s32MemID, displayHandle, uEye.Defines.DisplayRenderMode.FitToWindow);
-		//}
+			cam.Image.Save(SAVE_PATH + "uEye_image_" + i + SAVE_PATH_SUFFIX, ImageFormat.Png);
+		}
 	}
 }
