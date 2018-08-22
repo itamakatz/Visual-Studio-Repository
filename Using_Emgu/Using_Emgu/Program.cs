71776e99fdeedcf8d4c0f8a33a509177c4b1c8b4
itamakatz <itamakatz@gmail.com>
2018-08-22 17:47:37 +0300

    started using the Emgu interface to OpenCV which worked well
    mainly did pano stoching and edge detection

--- Using_Emgu/Using_Emgu/Program.cs
+++ Using_Emgu/Using_Emgu/Program.cs
@@ -1,123 +1,168 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Emgu.CV;
 using Emgu.CV.Features2D;
+using Emgu.CV.Stitching;
 using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.CV.Util;
 
 namespace Using_Emgu {
 	class Program {
 
-		static Form form = new Form();
-		static OpenFileDialog Openfile = new OpenFileDialog();
+		static Form Compare_Form = new Form();
+		static OpenFileDialog Open_File = new OpenFileDialog();
+		static SaveFileDialog Save_File = new SaveFileDialog();
 		static Image<Bgr, Byte> My_Image;
 
-		static PictureBox pictureBox_UL = new PictureBox();
-		static PictureBox pictureBox_UR = new PictureBox();
+		static ImageBox Image_Box_UL = new ImageBox();
+		static ImageBox Image_Box_UR = new ImageBox();
+		static ImageBox Image_Box_DL = new ImageBox();
+		static ImageBox Image_Box_DR = new ImageBox();
 
-		static PictureBox pictureBox_DL = new PictureBox();
-		static PictureBox pictureBox_DR = new PictureBox();
+		static Form Pano_Form = new Form();
+		static Image<Bgr, Byte> Pano_Image;
+		static ImageBox Pano_Image_Box = new ImageBox();
+		//static PictureBox Pano_Image_Box = new PictureBox();
 
 		[STAThread]
 		static void Main(string[] args) {
 
 			Init_Controls();
 
-			Open_Image();
+			//Create_Pano();
 
-			form.ShowDialog();
+			Compare_Images();
+
+			Compare_Form.ShowDialog();
+			//Pano_Form.ShowDialog();
 		}
 
-		static void Open_Image() {
-			Openfile.ShowDialog();
-			My_Image = new Image<Bgr, byte>(Openfile.FileName);
+		static void Compare_Images() {
+			My_Image = new Image<Bgr, byte>(@"C:\Users\admin\Desktop\COM_Integration\Panorama Stiching\Image Sets\pano_calibrate_3.jpg");
 
-			pictureBox_UL.Image = My_Image.ToBitmap();
+			Image_Box_UL.Image = My_Image;
+			Image_Box_UR.Image = My_Image.Canny(100, 200);
+			//Image_Box_DL.Image = My_Image.Canny(255, 255 / 4);
+			Image_Box_DL.Image = My_Image.Canny(255, 200);
+			Image_Box_DR.Image = My_Image.Laplace(1);
+			//Image_Box_UR.Image = My_Image.SmoothGaussian(21).ToBitmap();
+			//My_Image.Canny(5, 5);
+			//Image_Box_UR.Image = My_Image.SmoothGaussian(21).ToBitmap();
+		}
 
-			pictureBox_UR.Image = My_Image.SmoothGaussian(21).ToBitmap();
-			My_Image.Canny(5, 5);
-			pictureBox_UR.Image = My_Image.SmoothGaussian(21).ToBitmap();
+		static void Create_Pano() {
+			using (Stitcher stitcher = new Stitcher(Stitcher.Mode.Panorama, false)) {
+
+				using (VectorOfMat vm = new VectorOfMat()) {
+
+					Open_File.Multiselect = true;
+					Open_File.ShowDialog();
+
+					foreach (string fileName in Open_File.FileNames) {
+						vm.Push(new Mat(fileName));
+					}
+
+					Mat result = new Mat();
+					stitcher.Stitch(vm, result);
+					Pano_Image_Box.Image = result;
+				}
+			}
 		}
 
 		static void Init_Controls() {
 
-			form.Width = 800;
-			form.Height = 800;
+			// Pano_Form //
 
-			form.Controls.Add(pictureBox_UL);
-			form.Controls.Add(pictureBox_UR);
-			form.Controls.Add(pictureBox_DL);
-			form.Controls.Add(pictureBox_DR);
+			Pano_Form.Height = Screen.PrimaryScreen.Bounds.Height;
+			Pano_Form.Width = Screen.PrimaryScreen.Bounds.Width;
+			Pano_Form.Controls.Add(Pano_Image_Box);
 
-			form.SizeChanged += PictureBox_Resize;
+			Pano_Image_Box.SizeMode = PictureBoxSizeMode.Zoom;
+			Pano_Image_Box.Size = Pano_Form.Size;
+			Pano_Image_Box.MouseClick += new MouseEventHandler(Pano_MouseMove);
 
-			pictureBox_UR.DoubleClick += PictureBox_DoubleClick;
-			pictureBox_UL.DoubleClick += PictureBox_DoubleClick;
+			// Compare_Form //
 
-			pictureBox_UL.SizeMode = PictureBoxSizeMode.Zoom;
-			pictureBox_UR.SizeMode = PictureBoxSizeMode.Zoom;
+			Compare_Form.Height = Screen.PrimaryScreen.Bounds.Height;
+			Compare_Form.Width = Screen.PrimaryScreen.Bounds.Width;
+
+			Compare_Form.SizeChanged += PictureBox_Resize;
+
+			PictureBox_Resize(null, null);
+
+			Image_Box_UR.DoubleClick += PictureBox_DoubleClick;
+			Image_Box_UL.DoubleClick += PictureBox_DoubleClick;
+			Image_Box_DL.DoubleClick += PictureBox_DoubleClick;
+			Image_Box_DR.DoubleClick += PictureBox_DoubleClick;
+
+			Compare_Form.Controls.Add(Image_Box_UL);
+			Compare_Form.Controls.Add(Image_Box_UR);
+			Compare_Form.Controls.Add(Image_Box_DL);
+			Compare_Form.Controls.Add(Image_Box_DR);
+
+			Image_Box_UL.SizeMode = PictureBoxSizeMode.Zoom;
+			Image_Box_UR.SizeMode = PictureBoxSizeMode.Zoom;
+			Image_Box_DL.SizeMode = PictureBoxSizeMode.Zoom;
+			Image_Box_DR.SizeMode = PictureBoxSizeMode.Zoom;
 		}
 
 		static void PictureBox_Resize(object sender, EventArgs e) {
-			if(pictureBox_UL == null) { return; }
+			if (Image_Box_UL == null) { return; }
 
-			int offset = 0;
+			Image_Box_UL.Height = Compare_Form.Height / 2 - 15;
+			Image_Box_UR.Height = Compare_Form.Height / 2 - 15;
+			Image_Box_DL.Height = Compare_Form.Height / 2 - 15;
+			Image_Box_DR.Height = Compare_Form.Height / 2 - 15;
 
-			if (form.Width % 2 != 0) { offset = 1; }
+			Image_Box_UL.Width = Compare_Form.Width / 2 - 10;
+			Image_Box_UR.Width = Compare_Form.Width / 2 - 10;
+			Image_Box_DL.Width = Compare_Form.Width / 2 - 10;
+			Image_Box_DR.Width = Compare_Form.Width / 2 - 10;
 
-			pictureBox_UL.Height = form.Height / 2 - 1;
-			pictureBox_UR.Height = form.Height / 2 - 1;
-			pictureBox_DL.Height = form.Height / 2 - 1;
-			pictureBox_DR.Height = form.Height / 2 - 1;
-
-			pictureBox_UL.Width = form.Width / 2 - 1;
-			pictureBox_UR.Width = form.Width / 2 - 1;
-			pictureBox_DL.Width = form.Width / 2 - 1;
-			pictureBox_DR.Width = form.Width / 2 - 1;
-
-			pictureBox_UL.Location = new Point(form.Width / 2, 0);
-			pictureBox_UR.Location = new Point(form.Width / 2, 0);
-			pictureBox_DL.Location = new Point(form.Width / 2, 0);
-			pictureBox_DR.Location = new Point(form.Width / 2, 0);
-
-			pictureBox_UR.Location = new Point((int) Math.Ceiling((double) form.Width / 2) + 1, 0);
-
-
-			pictureBox_UL.Width = (int) Math.Floor((double) form.Width / 2);
-
-			if (form.Width % 2 == 0) {
-				pictureBox_UR.Width = (int) Math.Ceiling((double) form.Width / 2) + 1;
-			} else {
-				pictureBox_UR.Width = (int) Math.Ceiling((double) form.Width / 2);
-				pictureBox_UR.Location = new Point((int) Math.Ceiling((double) form.Width / 2), 0);
-			}
+			Image_Box_UL.Location = new Point(0, 0);
+			Image_Box_UR.Location = new Point(Compare_Form.Width / 2 - 5, 0);
+			Image_Box_DL.Location = new Point(0, Compare_Form.Height / 2 - 10);
+			Image_Box_DR.Location = new Point(Compare_Form.Width / 2 - 5, Compare_Form.Height / 2 - 10);
 		}
 
 		static void PictureBox_DoubleClick(object sender, EventArgs e) {
 
-			if (pictureBox_UL.SizeMode == PictureBoxSizeMode.Zoom) {
-				pictureBox_UL.SizeMode = PictureBoxSizeMode.CenterImage;
-				pictureBox_UR.SizeMode = PictureBoxSizeMode.CenterImage;
+			if (Image_Box_UL.SizeMode == PictureBoxSizeMode.Zoom) {
+				Image_Box_UL.SizeMode = PictureBoxSizeMode.CenterImage;
+				Image_Box_UR.SizeMode = PictureBoxSizeMode.CenterImage;
+				Image_Box_DL.SizeMode = PictureBoxSizeMode.CenterImage;
+				Image_Box_DR.SizeMode = PictureBoxSizeMode.CenterImage;
 			} else {
-				pictureBox_UL.SizeMode = PictureBoxSizeMode.Zoom;
-				pictureBox_UR.SizeMode = PictureBoxSizeMode.Zoom;
+				Image_Box_UL.SizeMode = PictureBoxSizeMode.Zoom;
+				Image_Box_UR.SizeMode = PictureBoxSizeMode.Zoom;
+				Image_Box_DL.SizeMode = PictureBoxSizeMode.Zoom;
+				Image_Box_DR.SizeMode = PictureBoxSizeMode.Zoom;
 			}
 		}
 
 		//DataGridView myNewGrid = new DataGridView();
 
-		//pictureBox_Left.MouseMove += new MouseEventHandler(pb_MouseMove);
-		//pictureBox_Right.MouseMove += new MouseEventHandler(pb_MouseMove);
+		//Image_Box_Left.MouseMove += new MouseEventHandler(pb_MouseMove);
+		//Image_Box_Right.MouseMove += new MouseEventHandler(pb_MouseMove);
 
 		//static void pb_MouseMove(object sender, MouseEventArgs e) {
-		//	if (e.Button == MouseButtons.Left && pictureBox_Left.SizeMode == PictureBoxSizeMode.CenterImage) {
-		//		pictureBox_Left.Left = e.X;
-		//		pictureBox_Left.Top = e.Y;
+		//	if (e.Button == MouseButtons.Left && Image_Box_Left.SizeMode == PictureBoxSizeMode.CenterImage) {
+		//		Image_Box_Left.Left = e.X;
+		//		Image_Box_Left.Top = e.Y;
 		//	}
 		//}
+
+		static void Pano_MouseMove(object sender, MouseEventArgs e) {
+			if (e.Button == MouseButtons.Left) {
+				Create_Pano();
+			}
+		}
 	}
 }
